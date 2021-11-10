bd3fbc1853f7f0f68ba1821cf5b6fda2a27aa1b6
Diego Santoni <50447464+diegosantoni@users.noreply.github.com>
2021-11-10 12:02:40 -0300

    Update recreate.sh

--- recreate.sh
+++ recreate.sh
@@ -22,41 +22,5 @@ sudo snap install microk8s --classic
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 
+echo "Now excecute recreate2.sh"
 su - $USER
-
-# Enable Microk8s addons
-
-microk8s.enable registry
-microk8s.enable dns
-microk8s.enable helm3
-microk8s.enable metallb:$range
-
-# For metallb IP Range, choose a short range of your local network
-
-# Clone git repo and switch to master branch
-
-git clone https://github.com/diegosantoni/traackr.git
-
-git checkout master
-
-# Build docker image
-
-docker build -t traackr ./traackr/
-
-# Push de docker image to microk8s registry on localhost
-
-docker tag traackr:latest localhost:32000/traackr:latest
-docker push localhost:32000/traackr:latest
-
-# Install Traefik
-
-microk8s helm3 repo add traefik https://helm.traefik.io/traefik
-microk8s helm3 repo update
-microk8s kubectl create namespace traefik
-microk8s helm3 install traefik traefik/traefik -n traefik
-
-
-# Install server using HELM
-
-microk8s helm3 install web-traackr ./traackr/helm/traackrweb
-
