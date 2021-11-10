838856a5d8006246d8d986ac7559a786ba404b87
Diego Santoni <50447464+diegosantoni@users.noreply.github.com>
2021-11-10 14:30:54 -0300

    Update recreate2.sh

--- recreate2.sh
+++ recreate2.sh
@@ -27,19 +27,7 @@ git checkout master
 # Build docker image
 
 docker build -t traackr .
-
-# Push de docker image to microk8s registry on localhost
-
-echo "Waiting 20s for addons to start"
-sleep 20
-
 docker tag traackr:latest localhost:32000/traackr:latest
-
-for n in {1..3}; do
-    sudo docker push  localhost:32000/traackr:latest && break;
-done
-
-# sudo docker push localhost:32000/traackr:latest
 
 # Install Traefik
 
@@ -47,10 +35,3 @@ microk8s helm3 repo add traefik https://helm.traefik.io/traefik
 microk8s helm3 repo update
 microk8s kubectl create namespace traefik
 microk8s helm3 install traefik traefik/traefik -n traefik --set service.spec.loadBalancerIP=$staticIP
-
-
-# Install server using HELM
-
-sudo docker push localhost:32000/traackr:latest # Some times the first attemp fails, so re-try.
-sleep 3
-microk8s helm3 install web-traackr ./helm/traackrweb
