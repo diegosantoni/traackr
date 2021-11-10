0277c83866937c57d679bf6da97cefdb07e76f2a
Diego Santoni <50447464+diegosantoni@users.noreply.github.com>
2021-11-10 14:49:37 -0300

    Update recreate3.sh

--- recreate3.sh
+++ recreate3.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Push image to microk8s local registry
+# Push image to microk8s local registry and install HELM chart
 
 for n in {1..3}; do
-    docker push  localhost:32000/traackr:latest && break;
+    docker push  localhost:32000/traackr:latest && microk8s helm3 install web-traackr ./helm/traackrweb && break;
 done
-
-# Install server using HELM
-
-sudo docker push localhost:32000/traackr:latest # Some times the first attemp fails, so re-try.
-sleep 3
-microk8s helm3 install web-traackr ./helm/traackrweb
